0b4fdb813f58ede1910da9e9a874edf0217f9170
pz1 <pz3@xs4all.nl>
2015-11-12 13:02:31 +0100

    Initial commit
    JSONPath is not functioning yet

---
+++ TTNLoraWAN/index.js
@@ -0,0 +1,101 @@
+/*** TTNLoraWAN Z-Way HA module *******************************************
+
+Version: 0.0.1
+(c) Z-Wave.Me, 2014
+-----------------------------------------------------------------------------
+Author: Pieter E. Zanstra adaptation for TTN-LoraWAN
+Derived from OpenWeather module by Serguei Poltorak <ps@z-wave.me>
+Description:
+This module creates ??EXPERIMENTAL??
+
+ ******************************************************************************/
+
+// ----------------------------------------------------------------------------
+// --- Class definition, inheritance and setup
+// ----------------------------------------------------------------------------
+
+function TTNLoraWAN(id, controller) {
+	// Call superconstructor first (AutomationModule)
+	TTNLoraWAN.super_.call(this, id, controller);
+}
+
+inherits(TTNLoraWAN, AutomationModule);
+
+_module = TTNLoraWAN;
+
+// ----------------------------------------------------------------------------
+// --- Module instance initialized
+// ----------------------------------------------------------------------------
+
+TTNLoraWAN.prototype.init = function (config) {
+	TTNLoraWAN.super_.prototype.init.call(this, config);
+
+	var self = this;
+
+	this.vDev = self.controller.devices.create({
+			deviceId : "TTNLoraWAN_" + this.id,
+			defaults : {
+				deviceType : "sensorMultilevel",
+				metrics : {
+					probeTitle : this.config.nodetitle
+				}
+			},
+			overlay : {
+				metrics : {
+					scaleTitle : '°C',
+					title : this.config.node_eui
+				}
+			},
+			moduleId : this.id
+		});
+
+	this.timer = setInterval(function() {
+		self.fetchTTNSensor(self);
+	}, 900*1000);
+	self.fetchTTNSensor(self);
+};
+
+TTNLoraWAN.prototype.stop = function () {
+	TTNLoraWAN.super_.prototype.stop.call(this);
+
+	if (this.timer)
+		clearInterval(this.timer);
+
+	if (this.vDev) {
+		this.controller.devices.remove(this.vDev.id);
+		this.vDev = null;
+	}
+};
+
+// ----------------------------------------------------------------------------
+// --- Module methods
+// ----------------------------------------------------------------------------
+
+TTNLoraWAN.prototype.fetchTTNSensor = function (instance) {
+	var self = instance,
+	moduleName = "TTNLoraWAN",
+	langFile = self.controller.loadModuleLang(moduleName);
+
+	http.request({
+		url : self.config.url + self.config.node_eui + "/?format=json" ,
+		async : true,
+		success : function (res) {
+			try {
+				var temp = res.data.data_json.temperature,
+				nodetitle = res.data.data_json.node,
+				observe_time = res.data.time;
+
+				self.vDev.set("metrics:level", temp);
+				self.vDev.set("metrics:name", nodetitle);
+				self.vDev.set("metrics:observe_time", observe_time);
+				self.vDev.set("metrics:icon","http://192.168.1.28:8083/ZAutomation/api/v1/load/modulemedia/TTNLoraWAN/icon.png");
+			} catch (e) {
+				self.controller.addNotification("error", langFile.err_parse, "module", moduleName);
+			}
+		},
+		error : function () {
+			self.controller.addNotification("error", langFile.err_fetch, "module", moduleName);
+			console.log ("TTNurl: ", url);
+		}
+	});
+};
