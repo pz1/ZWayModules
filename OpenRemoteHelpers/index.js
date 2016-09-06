8d3350ecc67ddabb9c9063926d0df59b38b4f3f0
pz1 <pz3@xs4all.nl>
2016-09-06 17:01:47 +0200

    OpenRemoteHelpers Moved License text to READ.me

--- OpenRemoteHelpers/index.js
+++ OpenRemoteHelpers/index.js
@@ -4,23 +4,7 @@
  * Adopted for Z-Wave.Me Z-Way
  * Copyright 2008-2013, OpenRemote Inc.
  *
- * See the contributors.txt file in the distribution for a
- * full listing of individual contributors.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * See README.MD for instructions
+ * See README.MD for instructions and license terms
  */
 
 // ----------------------------------------------------------------------------
